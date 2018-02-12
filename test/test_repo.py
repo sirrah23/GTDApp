89a2d2e704e9e030456353582e48ba4ae0cc17a7
sirrah23 <hseth93@gmail.com>
2018-02-11 22:10:20 -0500

    Added test for project storage

--- test/test_repo.py
+++ test/test_repo.py
@@ -24,3 +24,16 @@ def test_store_task():
     assert tasks[0].description == "This is a task"
     assert tasks[0].status == "todo"
     drop_db(DBNAME)
+
+def test_store_project():
+    drop_db(DBNAME)
+    GTDRepo.add_task(description="Task1")
+    GTDRepo.add_task(description="Task2")
+    tasks = list(map(lambda t: t.id, GTDRepo.get_all_tasks()))
+    GTDRepo.add_project(description="ProjectA",  tasks=tasks)
+    projects = GTDRepo.get_all_projects()
+    assert len(projects) == 1
+    assert projects[0].description == "ProjectA"
+    assert len(projects[0].tasks) == 2
+    #assert projects[0].tasks[0] in tasks
+    #assert projects[0].tasks[1] in tasks
