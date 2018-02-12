b813ab7c7d3e274eac1e77572c375522a687d791
sirrah23 <hseth93@gmail.com>
2018-02-11 22:02:02 -0500

    Added tests for actual db contents of Item and Task

--- test/test_repo.py
+++ test/test_repo.py
@@ -9,12 +9,18 @@ def test_store_item():
     drop_db(DBNAME)
     GTDRepo.connect(DBNAME)
     GTDRepo.add_item(description="This is an item", location="inbox")
-    assert len(GTDRepo.get_all_items()) == 1
+    items = GTDRepo.get_all_items()
+    assert len(items) == 1
+    assert items[0].description == "This is an item"
+    assert items[0].location == "inbox"
     drop_db(DBNAME)
 
 def test_store_task():
     drop_db(DBNAME)
     GTDRepo.connect(DBNAME)
     GTDRepo.add_task(description="This is a task")
-    assert len(GTDRepo.get_all_tasks()) == 1
+    tasks = GTDRepo.get_all_tasks()
+    assert len(tasks) == 1
+    assert tasks[0].description == "This is a task"
+    assert tasks[0].status == "todo"
     drop_db(DBNAME)
