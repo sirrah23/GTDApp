aa7fe9e61d8cfee60760219ad01b909743c7b43b
sirrah23 <hseth93@gmail.com>
2018-02-26 22:48:50 -0500

    Moved remaining view stuff to index.py and added redirect
    There was some remaining view logic that we didn't really
    need in __init__.py so I moved it to index.py.
    
    When the user is logged in they should not able to go to
    the login or registration pages so I added some redirects
    to prevent that.

--- GTDApp/__init__.py
+++ GTDApp/__init__.py
@@ -1,53 +1,20 @@
-from flask import Flask, request, render_template, redirect
-from flask_login import LoginManager, login_user, login_required, UserMixin, logout_user, current_user
+from flask import Flask
+from flask_login import LoginManager
 from GTDApp.repo import GTDRepo
 
 
+# Initialize the app
 app = Flask(__name__)
 app.secret_key = "myspookysecret"
 
 
+# Initialize the login manager for the app
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+# Connect to the database
 GTDRepo.connect("gtd") # TODO: config
-
-
-class LoggedInUserWrapper(UserMixin):
-
-    def __init__(self, u):
-        self.u = u
-
-    def get_id(self):
-        if not self.u:
-            return None
-        return str(self.u.id)
-
-    def get_obj_id(self):
-        if not self.u:
-            return None
-        return self.u.id
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return LoggedInUserWrapper(GTDRepo.get_user_by_id(user_id))
-
-
-# Route for handling the login page logic
-@app.route("/", methods=["GET", "POST"])
-def login():
-    error = None
-    if request.method == "POST":
-        user = GTDRepo.get_user_by_username(request.form["username"])
-        if user and user.password == request.form["password"]:
-            user = LoggedInUserWrapper(user)
-            login_user(user)
-            return redirect("/home")
-        else:
-            error = "Invalid Credentials. Please try again."
-    return render_template("login.html", error=error)
 
 
 from GTDApp.views import index
--- GTDApp/views/index.py
+++ GTDApp/views/index.py
@@ -1,8 +1,29 @@
-from GTDApp import app
-from GTDApp.repo import GTDRepo
-from flask import request, render_template
-from flask_login import login_required, logout_user
+from flask import request, render_template, redirect
+from flask_login import login_user, login_required, logout_user, UserMixin, current_user
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from GTDApp import app, login_manager
+from GTDApp.repo import GTDRepo
+
+
+class LoggedInUserWrapper(UserMixin):
+
+    def __init__(self, u):
+        self.u = u
+
+    def get_id(self):
+        if not self.u:
+            return None
+        return str(self.u.id)
+
+    def get_obj_id(self):
+        if not self.u:
+            return None
+        return self.u.id
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return LoggedInUserWrapper(GTDRepo.get_user_by_id(user_id))
 
 
 class RegistrationForm(Form):
@@ -15,8 +36,27 @@ class RegistrationForm(Form):
     confirm = PasswordField('Repeat Password')
 
 
+# Route for handling the login page logic
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if current_user and current_user.is_authenticated:
+        return redirect("/home")
+    error = None
+    if request.method == "POST":
+        user = GTDRepo.get_user_by_username(request.form["username"])
+        if user and user.password == request.form["password"]:
+            user = LoggedInUserWrapper(user)
+            login_user(user)
+            return redirect("/home")
+        else:
+            error = "Invalid Credentials. Please try again."
+    return render_template("login.html", error=error)
+
+
 @app.route('/register', methods=["GET", "POST"])
 def register():
+    if current_user and current_user.is_authenticated:
+        return redirect("/home")
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
         print(form.username.data, form.email.data,
