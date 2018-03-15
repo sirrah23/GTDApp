688962d6c5836c69b9d0445e1c8bde6849d3b5bb
sirrah23 <hseth93@gmail.com>
2018-03-15 01:22:18 -0400

    UI test to add and then delete an item

--- test/test_functional.py
+++ test/test_functional.py
@@ -10,6 +10,9 @@ from GTDApp.repo import UserRepo
 from utils import drop_db
 
 
+# TODO: Extract login code in four-five function tests
+
+
 URL = "http:localhost:5000"  # Location of our application
 DB = app.config["DBNAME"]  # Database containing our test data
 
@@ -133,7 +136,7 @@ class TestLoginAndGo:
         screenforward_elem.click()
         screenforward_elem.click()
         screenforward_elem.click()
-        # Find the input where we can enter tasks
+        # Find the input where we can enter projects
         input_box = self.driver.find_element_by_tag_name("input")
         input_box.send_keys("Project 1")
         input_box.send_keys(Keys.RETURN)
@@ -150,6 +153,40 @@ class TestLoginAndGo:
         assert len(project_list_content) == 2
         assert project_list_content[0].startswith("Project 1") == True
         assert project_list_content[1].startswith("Project 2") == True
+
+    def test_log_in_and_delete_item(self):
+        self.driver.get(URL)
+        # Get username and password elements
+        userelem = self.driver.find_element_by_name("username")
+        passwordelem = self.driver.find_element_by_name("password")
+        # Type in username and password so we can log in
+        userelem.clear()
+        userelem.send_keys(self.username)
+        passwordelem.clear()
+        passwordelem.send_keys(self.password)
+        passwordelem.send_keys(Keys.RETURN)
+        try:
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, "app"))
+            )
+        except:
+            self.driver.quit()
+            drop_db(DB)
+            assert False  # Website did not load upon clicking link
+        # Find the input where we can enter items
+        input_box = self.driver.find_element_by_tag_name("input")
+        input_box.send_keys("Item 1")
+        input_box.send_keys(Keys.RETURN)
+        time.sleep(0.5) # Let the post request go through
+        # Make sure we have zero items, zero tasks, and one project
+        item_list_content = [el.text for el in self.driver.find_elements_by_class_name("gtd-item")]
+        assert len(item_list_content) == 1
+        # Delete the item we just added
+        item_delete_elems = self.driver.find_elements_by_class_name("itemdelete")
+        item_delete_elems[0].click()
+        time.sleep(0.5) # Let the post request go through
+        item_list_content = [el.text for el in self.driver.find_elements_by_class_name("gtd-item")]
+        assert len(item_list_content) == 0
 
     def teardown_method(self, method):
         # Closer the browser
